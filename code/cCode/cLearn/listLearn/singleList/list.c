018517dd4f7f20853e343b8c929f042f3a2724af
CCILU <826593711@qq.com>
2020-10-30 16:01:43 +0800

    add function

--- code/cCode/cLearn/listLearn/singleList/list.c
+++ code/cCode/cLearn/listLearn/singleList/list.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define success 0
-#define error   1
+#define error   -1
 
 typedef struct nodelist {
     int    data;
@@ -15,23 +16,83 @@ typedef struct nodelistFind {
     int         count;
 }nodeListFind,*nodeListFindPtr;
 
-int initList(nideListPtr headList) {
-    headList = (nideListPtr)malloc(sizeof(nodeList));
-    if(NULL == headList) {
+// int initList(nideListPtr *headList) {
+//     *headList = (nideListPtr)malloc(sizeof(nodeList));
+//     if(NULL == headList) {
+//         return error;
+//     }
+//     (*headList)->Next = NULL;
+//     return success;
+// }
+nideListPtr _initList() {
+    nideListPtr headList = (nideListPtr)malloc(sizeof(nodeList));
+    assert(headList);
+    return headList;   
+}
+
+int creatOneNodeAtHead(nideListPtr headList, int data) {
+    nideListPtr usrNodeList = (nideListPtr)malloc(sizeof(nodeList));
+    assert(usrNodeList);
+    if(headList == NULL) {
         return error;
     }
-    headList->Next = NULL;
+    usrNodeList->Next = headList->Next;
+    headList->Next = usrNodeList;
+    usrNodeList->data = data;
     return success;
 }
 
+void printfList(nideListPtr headList) {
+    int count = 1;
+    if(headList == NULL)
+        return;
+    while((headList = headList->Next) != NULL) {
+        printf("Node %d is: %d \t", count, headList->data);
+        count++;
+    }
+    printf("\n");
+    return;
+}
 
+int deleteNodeWithData(nideListPtr headList, int data) {
+    nideListPtr  headlist = headList;
+    unsigned int flag = 0;
+    if(headlist->Next == NULL) {
+        return error;
+    }
+    while(((headlist = headlist->Next) != NULL) && (headlist->data == data)) {
+        if(headlist->Next == NULL) {
+            free(headlist);
+            headlist = NULL;
+        }
+        nideListPtr tempoaryNode = headlist;
+        headlist = tempoaryNode->Next;
+        free(tempoaryNode);
+        tempoaryNode = NULL;
+        flag++;
+    }
+    if(flag > 0)
+        return flag;
+    return error;
+}
 
 int main() {
     int ret = 1;
-    nideListPtr headList;
-    ret = initList(headList);
-    printf("ret is %d \n", ret);
-
+    nideListPtr headList = _initList();
+    //ret = initList(&headList);
+    printf("initList ret is %d \n", ret);
+    ret = creatOneNodeAtHead(headList, 1);
+    printf("creatOneNodeAtHead ret is %d \n", ret);
+    ret = creatOneNodeAtHead(headList, 2);
+    printf("creatOneNodeAtHead ret is %d \n", ret);
+    ret = creatOneNodeAtHead(headList, 3);
+    printf("creatOneNodeAtHead ret is %d \n", ret);
+    ret = creatOneNodeAtHead(headList, 2);
+    printf("creatOneNodeAtHead ret is %d \n", ret);
+    printfList(headList); 
+    ret = deleteNodeWithData(headList, 2);
+    printf("deleteNodeWithData ret is %d \n", ret);
+    printfList(headList);
 
     return 0;
 }
