a33fbe2f5d47799a53a255ea621ce9aeb6d35567
CCILU <826593711@qq.com>
2020-11-23 17:21:02 +0800

    niuke

--- code/cCode/cLearn/niuke/removeRepeatAndorder/removeRepeatAndorder.c
+++ code/cCode/cLearn/niuke/removeRepeatAndorder/removeRepeatAndorder.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int *removeRepeatAndorder(int *numberData, int numberLen, int *outDataLen){
-    int *midData = (int*)malloc(numberLen);
+    int *midData = (int*)malloc(numberLen+1);
     int count = 0;
     for(int i = 0; i < numberLen; i++) {
         int flag = 1;
@@ -15,17 +16,18 @@ int *removeRepeatAndorder(int *numberData, int numberLen, int *outDataLen){
         if(flag) {
             midData[count] = numberData[i];
             count++;
-        }
-    }
-    for(int i = 0; i < count-1; i++) {
-        for(int j = 0; j < count-i-1; j++) {
-            if(midData[j] > midData[j+1]) {
-                int temp = midData[j];
-                midData[j] = midData[j+1];
-                midData[j+1] = temp;
+            for(int j = 0; j < count-1; j++) {
+                if(midData[j] > midData[j+1]) {
+                    int temp = midData[j];
+                    midData[j] = midData[j+1];
+                    midData[j+1] = temp;
+                }
             }
         }
     }
+    // for(int i = 0; i < count-1; i++) {
+
+    // }
     *outDataLen = count;
     return midData;
 }
@@ -39,17 +41,17 @@ int main() {
     int outDataLen = 0;
     int numberData[1000] = {0};
     int *p = NULL;
-    while(1) {
+   // while(1) {
         scanf("%d",&count);
-        if(count <= 0) {
-            break;
-        }
+        // if(count <= 0) {
+        //     break;
+        // }
         for(int i = 0; i < count; i++) {
             scanf("%d",&(numberData[numberLen]));
             numberLen++;
-           // printf("numberLen = %d\n",numberLen);
+            //printf("numberLen = %d\n",numberLen);
         }
-    }
+   // }
     p = removeRepeatAndorder(numberData, numberLen, &outDataLen);
     for(int i = 0; i < outDataLen; i++) {
         printf("%d\n",p[i]);
